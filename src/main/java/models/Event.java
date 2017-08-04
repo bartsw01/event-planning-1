2a399670d3317a4ca73e0c5a436fccc639844dcd
Maria Thomas <malethr@gmail.com>
2017-08-04 13:05:56 -0700

    add instance test of event and add constructor in event.java file

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,4 +4,16 @@ package models;
  * Created by mariathomas on 8/4/17.
  */
 public class Event {
+
+    private int numGuest;
+    private String typeFood;
+    private String typeBeverage;
+    private String typeEntertainment;
+
+    public Event(int guest, String food, String beverage, String entertainment) {
+        numGuest =  guest;
+        typeFood = food;
+        typeBeverage = beverage;
+        typeEntertainment = entertainment;
+    }
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -2,12 +2,17 @@ package models;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by mariathomas on 8/4/17.
  */
 public class EventTest {
 
     @Test
-
+    public void newEvent_instantiatesCorrectly() throws Exception {
+        Event testEvent = new Event (50, "Asian", "Non-alcohol", "Live Band");
+        assertEquals(true, testEvent instanceof Event);
+    }
 
 }
