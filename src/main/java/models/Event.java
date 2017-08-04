745df313644166c223a6efa2e0d37cab6d3705a4
Maria Thomas <malethr@gmail.com>
2017-08-04 11:54:33 -0700

    create initial java file

---
+++ src/main/java/models/Event.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by mariathomas on 8/4/17.
+ */
+public class Event {
+}
---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * Created by mariathomas on 8/4/17.
+ */
+public class EventTest {
+
+}
